3b205d7dba6e501fe23f473844c514a801fb49e7
Sam Pullman <sampullman@gmail.com>
2017-08-25 13:32:22 -0700

    Basic ability to update Fp and Fs

--- bass_calc.py
+++ bass_calc.py
@@ -4,9 +4,8 @@ import os, sys
 import math
 import numpy as np
 from scipy import signal
-import matplotlib.pyplot as plt
 from scipy.integrate import odeint
-
+from graphics import init_app, show_main, start_app
 
 #Equation from "Complete Response Function and System Parameters for a Loudspeaker with Passive Radiator" by Douglas H. Hurlburt
 
@@ -44,28 +43,76 @@ fs = 55.0 #Resonant Frequncy of Driver Hertz (hertz)
 Qs = 0.4 #Cas * Rras / Ts # Q of Driver 
 Qp = 10.0 #Cap * Rap / Tp # Q of Passive Radiator
 
+class Equation:
 
-#variables used in Equation
-Tp = (2.0 * math.pi * fp) ** 2.0
-Ts = (2.0 * math.pi * fs) ** 2.0
-y = fp/fs
-Γ =  0.2 #τb / Ts #0.2 is a good guesstimate
-ψ = α + δ + 1 
+    def __init__(self):
+        self.update()
+    
+    def update(self):
+        self.Tp = (2.0 * math.pi * fp) ** 2.0
+        self.Ts = (2.0 * math.pi * fs) ** 2.0
+        self.y = fp/fs
+        self.Γ =  0.2 #τb / Ts #0.2 is a good guesstimate
+        self.ψ = α + δ + 1
+    
+    def calculate(self):
+        return Equation.calculate_static(self.Tp, self.Ts, self.y, self.Γ, self.ψ)
 
-T0 = Ts / (math.sqrt(y) * math.pow(ψ,0.25)) #8a
-a1 = (math.sqrt(y) / math.pow(ψ,0.25)) * ((1 / Qp) + (1 / (y * Qs)) + (Γ * ((α / y) + (y * δ))))
-a2 = (1 / math.sqrt(ψ)) * (((α + 1) / y) + (y * (δ + 1)) + (1 / (Qp * Qs)) + (Γ *((α / Qp) + (y * (δ / Qs)))))
-a3 = (math.sqrt(y) / math.pow(ψ, 0.75)) * (((δ + 1) / Qs) + ((α + 1) / (y * Qp)) + (Γ * (α + δ)))
-b1 = math.sqrt(y) / (Qp * math.pow(ψ, 0.25))
-b2 = y / math.sqrt(ψ)
+    @staticmethod
+    def calculate_static(Tp, Ts, y, Γ, ψ):
+        T0 = Ts / (math.sqrt(y) * math.pow(ψ,0.25)) #8a
+        a1 = ((math.sqrt(y) / math.pow(ψ,0.25)) * 
+            ((1 / Qp) + (1 / (y * Qs)) + (Γ * ((α / y) + (y * δ)))))
 
-num = [1, b1, b2, 0, 0 ]
-den = [1, a1, a2, a3, 1]
+        a2 = ((1 / math.sqrt(ψ)) * (((α + 1) / y) +
+                                    (y * (δ + 1)) +
+                                    (1 / (Qp * Qs)) +
+                                    (Γ *((α / Qp) +
+                                    (y * (δ / Qs))))))
 
+        a3 = ((math.sqrt(y) / math.pow(ψ, 0.75)) *
+            (((δ + 1) / Qs) + ((α + 1) / (y * Qp)) + (Γ * (α + δ))))
 
-sys = signal.TransferFunction(num,den)
-print(sys)
-w, mag, phase = signal.bode(sys)
-plt.figure(1)
-plt.plot(w,mag,'b')
-plt.show()
+        b1 = math.sqrt(y) / (Qp * math.pow(ψ, 0.25))
+        b2 = y / math.sqrt(ψ)
+
+        num = [1, b1, b2, 0, 0 ]
+        den = [1, a1, a2, a3, 1]
+
+        sys = signal.TransferFunction(num, den)
+        print(sys)
+        return signal.bode(sys)
+
+main_equation = Equation()
+
+def vb_changed(main, value):
+    print("Vb updated: {}".format(value))
+    global Vb
+    Vb = 0.5 + float(value)
+    update(main)
+
+def fs_changed(main, value):
+    global fs
+    fs = int(value)
+    update(main)
+
+def fp_changed(main, value):
+    global fp
+    fp = int(value)
+    update(main)
+
+def update(main):
+    main_equation.update()
+    w, mag, phase = main_equation.calculate()
+    main.update_graph(w, mag, phase)
+
+def run():
+    init_app()
+    w, mag, phase = main_equation.calculate()
+    main = show_main(w, mag, phase)
+    main.set_fs_callback(fs_changed)
+    main.set_fp_callback(fp_changed)
+    start_app()
+
+if __name__ == '__main__':
+    run()
---
+++ graphics.py
@@ -0,0 +1,125 @@
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+from matplotlib.backend_bases import key_press_handler
+import tkinter as tk
+from tkinter import ttk
+
+LARGE_FONT= ("Verdana", 12)
+
+app = None
+
+class App(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        tk.Tk.wm_title(self, "Bass Visulization")
+
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand = True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+    def show_main(self, graph):
+        main = MainPage(self, graph)
+        main.grid(row=0, column=0, sticky="nsew")
+        main.pack()
+        main.tkraise()
+        return main
+
+    def exit(self):
+        self.quit()     # stops mainloop
+        self.destroy()  # this is necessary on Windows to prevent
+                        # Fatal Python Error: PyEval_RestoreThread: NULL tstate
+
+class MainPage(tk.Frame):
+
+    def __init__(self, parent, graph):
+        tk.Frame.__init__(self, parent)
+        self.app = parent
+        self.graph = graph
+        label = tk.Label(self, text="Bass Calculator!", font=LARGE_FONT)
+        label.pack(padx=10, pady=10)
+        
+        self.fs_scale = self.setup_scale("Fs ", 30, 100, 55)
+        self.fp_scale = self.setup_scale("Fp ", 10, 80, 35)
+
+        self.graph_canvas = FigureCanvasTkAgg(graph, self)
+        self.graph_canvas.show()
+        self.graph_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        toolbar = NavigationToolbar2TkAgg(self.graph_canvas, self)
+        toolbar.update()
+        #canvas._tkcanvas.grid(row=9,column=0)
+
+        button = tk.Button(master=self, text='Quit', command=self.app.exit)
+        button.pack(side=tk.BOTTOM)
+        self.graph_canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        def on_key_event(event):
+            print('you pressed %s' % event.key)
+            key_press_handler(event, self.graph_canvas, toolbar)
+
+        self.graph_canvas.mpl_connect('key_press_event', on_key_event)
+
+    def setup_scale(self, label, from_, to, default, resolution=1):
+        scaleFrame = tk.Frame(self)
+        scaleFrame.pack(side=tk.TOP)
+        scaleLabel = tk.Label(scaleFrame, text=label)
+        scaleLabel.pack(side=tk.LEFT, anchor=tk.S)
+
+        scale = tk.Scale(scaleFrame, from_=from_, to=to, orient=tk.HORIZONTAL, resolution=resolution)
+        scale.set(default)
+        scale.pack(side=tk.LEFT)
+        return scale
+
+    def set_fs_callback(self, callback):
+        def changed(value):
+            self.fs_scale_callback(self, value)
+        
+        self.fs_scale_callback = callback
+        self.fs_scale.configure(command=changed)
+
+    def set_fp_callback(self, callback):
+        def changed(value):
+            self.fp_scale_callback(self, value)
+        
+        self.fp_scale_callback = callback
+        self.fp_scale.configure(command=changed)
+
+    def update_graph(self, w, mag, phase):
+        self.graph.update(w, mag, phase)
+        self.graph_canvas.draw()
+
+class BassGraph(Figure):
+
+    def __init__(self, w, mag, phase):
+        Figure.__init__(self, figsize=(8, 5), dpi=100)
+
+        self.w = w
+        self.mag = mag
+        self.phase = phase
+
+        self.plot = self.add_subplot(1, 1, 1)
+        self.plot.plot(w, mag, 'b')
+
+    def update(self, w, mag, phase):
+        self.w = w
+        self.mag = mag
+        self.phase = phase
+        self.plot.cla()
+        self.plot.plot(w, mag, 'b')
+
+def init_app():
+    global app
+    app = App()
+
+def show_main(w, mag, phase):
+    main = app.show_main(BassGraph(w, mag, phase))
+    return main
+
+def start_app():
+    app.mainloop()
