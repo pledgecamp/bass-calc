f60975f77ef33e5bc4762b1bc57ec578695e9cd6
John Fitz <johnfitz@berkeley.edu>
2017-11-29 11:51:51 -0800

    attempting to implement new equations

--- bass_calc.py
+++ bass_calc.py
@@ -50,6 +50,49 @@ class Equation:
         print(sys)
         return signal.bode(sys)
 
+class EquationAlternateFormulation:
+
+    def __init__(self):
+        self.update()
+    
+    def update(self):
+        self.Γ =  0.2 #τb / Ts # 0.2 is a good guesstimate
+        self.ψ = α + δ + 1
+    
+    def calculate(self):
+        return EquationAlternateFormulation.calculate_static(self.Γ, self.ψ)
+
+    @staticmethod
+    def calculate_static(Γ, ψ):
+
+        b4 = (Ts ** 2) * (Tp ** 2)
+        b3 = (Ts ** 2) * (Tp / Qmp)
+        b2 = (Ts ** 2)
+        b1 = 0
+        b0 = 0
+
+        a4 = (Ts ** 2) * (Tp ** 2)
+        a3 = (Ts ** 2) * Tp / Qmp + \
+                (Γ * Ts) * (α * (Tp ** 2) + (δ * (Ts ** 2))) 
+        a2 = (Tp ** 2) * (α + 1) + \
+                (Ts ** 2) * (δ + 1) + \
+                (Ts * Tp) / (Qs * Qmp) + \
+                (Γ * Ts) * ((α * Tp / Qmp) + (δ * Ts / Qs))
+        a1 = Ts * (δ + 1) / Qs + \
+                Tp * (α + 1) / Qmp + \
+                (Γ * Ts) * (α + δ)
+        a0 = ψ
+
+        num = [b4, b3, b2, b1, b0]
+        den = [a4, a3, a2, a1, a0]
+        print(num)
+        print(den)
+        
+        sys = signal.TransferFunction(num, den)
+        print(sys)
+        return signal.bode(sys)
+
+
 main_equation = Equation()
 main = None
 
